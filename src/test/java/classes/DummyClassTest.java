219a3fca2c2fa2790b8038c3558c1bab1a613666
Oğuzhan Toklu <oguztklu@gmail.com>
2020-02-21 09:10:11 +0300

    adds a dummy test class.

---
+++ src/test/java/classes/DummyClassTest.java
@@ -0,0 +1,14 @@
+package classes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class DummyClassTest {
+
+	@Test
+	public void testDummy() {
+		assertThat(true);
+	}
+	
+}
